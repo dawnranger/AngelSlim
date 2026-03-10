b33a8fbb62b852722d1aaa793e3bd79237df14b6
dawnranger <dawnranger123@gmail.com>
2026-03-10 11:58:38 +0800

    fix pyarrow offset overflow problem

--- angelslim/compressor/speculative/train/data/dataset_builder/online_dataset_builder.py
+++ angelslim/compressor/speculative/train/data/dataset_builder/online_dataset_builder.py
@@ -292,6 +292,7 @@ class OnlineVLMDatasetBuilder(OnlineDatasetBuilder):
                 "image_paths": json.dumps(image_paths),
             }
 
+
             return result_dict
 
         except Exception as e:
